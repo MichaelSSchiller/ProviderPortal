b1d7936d935a3c18ed05c4fabfb8b2af0e643caa
Michael Schiller <38842472+MichaelSSchiller@users.noreply.github.com>
2018-09-05 23:08:02 -0500

    Add files via upload

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, ParamMap} from '@angular/router';
 import { isNullOrUndefined } from 'util';
+declare var $: any;
 
 
 @Component({
